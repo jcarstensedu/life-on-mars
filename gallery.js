53f912038759e95f5857a9c181da170b810dc252
johnathan carstens <jcarstensedu@gmail.com>
2023-04-05 09:35:06 -0600

    finalised styles on homepage, gallery, and blog, for now,

--- gallery.js
+++ gallery.js
@@ -1,5 +1,17 @@
-function enlargeImg(img) {
-    img.style.transform = "scale(1.5)";
-    img.style.transition =
-        "transform 0.25s ease";
+var currentImgId;
+
+function enlargeImg(imgId) {
+    var img = document.getElementById(imgId);
+
+    // reset size of previously enlarged image, if any
+    if (currentImgId) {
+        var currentImg = document.getElementById(currentImgId);
+        currentImg.classList.remove("enlarged");
+    }
+
+    // enlarge clicked image
+    img.classList.add("enlarged");
+
+    // set current image id to clicked image id
+    currentImgId = imgId;
 }
